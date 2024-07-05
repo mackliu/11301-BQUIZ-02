6cbc15a4995a01094d25130d124553ec483e373d
kai <cgk0910@gmail.com>
2024-07-05 14:42:35 +0800

    完成最新文章的點擊顯示完整內容功能

--- frontend/news.php
+++ frontend/news.php
@@ -17,9 +17,14 @@
     foreach ($rows as $idx => $row) {
     ?>
         <tr>
-            <td><?= $row['title']; ?></td>
+            <td class='title'><?= $row['title']; ?></td>
             <td>
-                <?= mb_substr($row['article'], 0, 30); ?>
+                <div class='short'>
+                    <?= mb_substr($row['article'], 0, 30); ?>
+                </div>
+                <div class="all" style="display:none">
+                    <?= nl2br($row['article']); ?>
+                </div>
             </td>
             <td></td>
         </tr>
@@ -46,3 +51,8 @@
     ?>
 
 </div>
+<script>
+    $(".title").on("click", function() {
+        $(this).next().children(".short,.all").toggle();
+    })
+</script>
