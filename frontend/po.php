d6328561871e5e53d1e0e8c13c4b5961dd1b3093
kai <cgk0910@gmail.com>
2024-07-05 11:42:26 +0800

    建立分類網誌版型及機制

---
+++ frontend/po.php
@@ -0,0 +1,31 @@
+<div>
+    目前位置：首頁 > 分類網誌 > <span id='navType'>健康新知</span>
+</div>
+<div style='display: flex;align-items:start' ;>
+    <fieldset style='padding:10px;width:150px;'>
+        <legend>分類網誌</legend>
+        <a class="type" data-type="1" style="display:block;margin:5px;">健康新知</a>
+        <a class="type" data-type="2" style="display:block;margin:5px;">菸害防治</a>
+        <a class="type" data-type="3" style="display:block;margin:5px;">癌症防治</a>
+        <a class="type" data-type="4" style="display:block;margin:5px;">慢性病防治</a>
+    </fieldset>
+    <fieldset style='padding:10px;width:550px;'>
+        <legend id='newsTitle'>文章列表</legend>
+    </fieldset>
+</div>
+
+
+<script>
+    $(".type").on("click", function() {
+        $("#navType").text($(this).text())
+        getTitles($(this).data('type'))
+    })
+
+    function getTitles(type) {
+        console.log(type)
+    }
+
+    function getNews(id) {
+
+    }
+</script>
